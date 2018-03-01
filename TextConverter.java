410d2049648450530cbef3ddfd6dea2aeb9f7cbd
carlos meneses <cpmeneses94@gmail.com>
2018-03-01 13:23:48 -0300

    text_converter
    eso

---
+++ TextConverter.java
@@ -0,0 +1,12 @@
+package dada_t2_3;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class TextConverter {
+  public String read_text(String intxt) throws FileNotFoundException{
+    FileReader in;
+    in = new FileReader(intxt);
+    
+  }
+}
